22594644615519e6ef62cfb83ffca1e51e8238cb
Konstantinos Gkiotsalitis <49974094+KGkiotsalitis@users.noreply.github.com>
2019-11-07 13:55:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ print("x1=",sol['x[1]'])
 print("x2=",sol['x[2]'])
 print("x3=",sol['x[3]'])
 print("ksi=",sol['ksi'])
+
+print("h12=",sol['h12'])
+print("h13=",sol['h13'])
+print("h14=",sol['h14'])
+print("h22=",sol['h22'])
+print("h23=",sol['h23'])
+print("h24=",sol['h24'])
+print("h32=",sol['h32'])
+print("h33=",sol['h33'])
+print("h34=",sol['h34'])
 # Plot results
 """
 import matplotlib
